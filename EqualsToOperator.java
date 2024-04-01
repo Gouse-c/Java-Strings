b6bc32a4aa1def46560899d02657d514b4414f61
Gouse-c <140190450+Gouse-c@users.noreply.github.com>
2024-04-01 23:10:10 +0530

    Add files via upload

---
+++ EqualsToOperator.java
@@ -0,0 +1,16 @@
+class EqualsToOperator
+{
+	public static void main(String args[])
+	{
+		String str1 = "ABC";
+		String str2 = "ABC";
+		if(str1.equals(str2))
+		{
+			System.out.println("Both Strings are equal ...");
+		}
+		else
+		{
+			System.out.println("Both Strings are not equal...");
+		}
+	}
+}
