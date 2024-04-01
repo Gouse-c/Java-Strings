78a2d3b9c33fed0fd41e753317a4123f8f037138
Gouse-c <140190450+Gouse-c@users.noreply.github.com>
2024-04-01 23:14:26 +0530

    Add files via upload

---
+++ Replace.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Replace
+{
+	public static void main(String args[])
+	{
+		String str="Surendra Babu";
+		String res=str.replace('b','h');
+		System.out.println(res);
+	}
+}
